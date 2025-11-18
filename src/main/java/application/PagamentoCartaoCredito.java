63849a41f27ac38b5c722c42a6ca40b3c5a1b8f6
luisciaramicoli <161661669+luisciaramicoli@users.noreply.github.com>
2025-11-18 00:42:52 +0000

    pagamentoCartaoCredito

--- src/main/java/application/PagamentoCartaoCredito.java
+++ src/main/java/application/PagamentoCartaoCredito.java
@@ -5,18 +5,20 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class PagamentoCartaoCredito extends Pagamento {
+public class PagamentoCartaoCredito extends Pagamento implements Priorizavel, Processavel {
     private String numeroCartao;
     private String titularCartao;
     private String validadeCartao;
     private String cvv;
+    private int prioridade;
 
-    public PagamentoCartaoCredito(double valor, String descricao, String numeroCartao, String titularCartao, String validadeCartao, String cvv){
+    public PagamentoCartaoCredito(double valor, String descricao, String numeroCartao, String titularCartao, String validadeCartao, String cvv, int prioridade){
         super(valor,descricao);
         this.numeroCartao = numeroCartao;
         this.titularCartao = titularCartao;
         this.validadeCartao = validadeCartao;
         this.cvv= cvv;
+        this.prioridade = prioridade;
     }
     @Override
     public boolean autenticar(){
@@ -28,5 +30,32 @@ public class PagamentoCartaoCredito extends Pagamento {
     public boolean validarDados(){
         System.out.println("Validando dados do cartao para transação " + this.getIdTransacao());
         return true;
+    }
+
+    @Override
+    public int obterNivelPrioridade(){
+        return this.prioridade;
+    }
+
+    @Override
+    public void definirPrioridade(int prioridade){
+        if(prioridade >= 1 && prioridade <=10){
+            this.prioridade = prioridade;
+        }else{
+            System.out.println("Nivel de Prioridade Invalido (1-10)");
+        }
+    }
+
+    @Override
+    public boolean processar(){
+        System.out.println("Processando pagamento via cartão ID: " + this.getIdTransacao());
+        System.out.println("Transação "+this.getIdTransacao()+ " APROVADO");
+        this.setStatus("APROVADO");
+        return true;
+    }
+
+    @Override
+    public  String obterStatusProcessamento(){
+        return "Processamento via cartão "+ this.getIdTransacao() + " : " + this.getStatus();
     }
 }
